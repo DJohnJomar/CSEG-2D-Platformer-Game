3f7da3bb9e34d713d035e3a7698499a35ced099c
John Jomar <johndjomar@gmail.com>
2024-04-24 16:58:25 +0800

    Added scuffed but working parallax effect

--- Assets/Scripts/Parallax.cs
+++ Assets/Scripts/Parallax.cs
@@ -1,40 +1,24 @@
-using System.Collections;
-using System.Collections.Generic;
-using System.Runtime.CompilerServices;
 using UnityEngine;
 
 public class Parallax : MonoBehaviour
 {
+    public float parallaxEffectMultiplier = 0.5f;
+    private Transform cameraTransform;
+    private Vector3 lastCameraPosition;
 
-    private float length;
-    private float startpos;
-    public GameObject cam;
-    public float parallaxEffect;
-    // Start is called before the first frame update
     void Start()
     {
-        startpos = transform.position.x;
-        length = GetComponent<SpriteRenderer>().bounds.size.x;
+        cameraTransform = Camera.main.transform;
+        lastCameraPosition = cameraTransform.position;
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
-    }
+        float deltaX = cameraTransform.position.x - lastCameraPosition.x;
+        float deltaY = cameraTransform.position.y - lastCameraPosition.y;
 
-    private void FixedUpdate()
-    {
-        float temp = (cam.transform.position.x * (1 - parallaxEffect));
-        float distance = (cam.transform.position.x * parallaxEffect);
-        transform.position = new Vector3(startpos+distance, transform.position.y, transform.position.z);
+        transform.position += new Vector3(deltaX * parallaxEffectMultiplier, deltaY * parallaxEffectMultiplier, 0f);
 
-        if(temp> startpos + length)
-        {
-            startpos += length;
-        }else if(temp < startpos - length)
-        {
-            startpos -= length;
-        }
+        lastCameraPosition = cameraTransform.position;
     }
 }
